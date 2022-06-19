5a105a77bd924266d2cd17129512bbfb95a5bb4f
Leonit <93375344+LeonitRe@users.noreply.github.com>
2022-06-19 21:50:27 +0200

    BestTrainer_CRUD_REACTJS

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TrainerList from './components/TrainerList';
+
+function App() {
+  return (
+    <div className="container">
+      <TrainerList />
+    </div>
+  );
+}
+
+export default App;
